f0b10090cb1dae9b84f20fe33380ae95d1d7d8d3
chenpeng <chenpeng616@126.com>
2015-06-10 16:35:26 +0800

    新建工程

---
+++ First/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  First
+//
+//  Created by Ryan on 15/6/10.
+//  Copyright (c) 2015年 _MyCompany__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ First/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  First
+//
+//  Created by Ryan on 15/6/10.
+//  Copyright (c) 2015年 _MyCompany__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
